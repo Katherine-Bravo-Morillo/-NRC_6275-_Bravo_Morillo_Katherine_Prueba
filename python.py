1d27754a5fb358480648d31a4d616a5227c26516
Katherine Bravo <rkbravo@espe.edu.ec>
2022-05-31 08:38:39 -0500

    Segunda modificacion

--- python.py
+++ python.py
@@ -11,6 +11,18 @@ app = Flask(__name__, template_folder='templates')
 #Funcion utilizada para hacer un llamado al index
 def index():
     return render_template('index.html')
+
+@app.route('/registro_Cuenta')
+
+#Funcion para llamar al archivo de registro de cuenta
+def registro_Cuenta():
+    return render_template('/registro_Cuenta.html')
+
+@app.route('/iniciar_Seccion')
+
+#Funcion para llamar al archivo de inicio de seccion
+def iniciar_Seccion():
+    return render_template('/iniciar_Seccion.html')
 #iniciamos la aplicacion
 if __name__ == '__main__':
     app.run(debug=True)
